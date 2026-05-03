9164bd3660c2a3051866c2f625d79c175c8e7db9
Paweł Wlazło <pawel@wlazlo.dev>
2026-05-03 19:03:05 +0200

    feat: migrate mini.notify->snacks.notifier, mini.cursorword->snacks.words

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,4 +2,27 @@
 -- SNACKS.NVIM
 -- ============================================================================
 
-require("snacks").setup({})
+require("snacks").setup({
+  notifier = {
+    enabled = true,
+    style = "fancy",
+    timeout = 3000,
+    icons = {
+      error = " ",
+      warn = " ",
+      info = " ",
+      debug = " ",
+      trace = "✎",
+    },
+  },
+  words = {
+    enabled = true,
+    debounce = 200,
+    notify_jump = false,
+    notify_end = false,
+  },
+})
+
+vim.keymap.set("n", "<leader>n", function()
+  Snacks.notifier.show_history()
+end, { desc = "Notification history" })
