cc244478a830bdad803514dee9815b2ccca7dd53
Paweł Wlazło <pawel@wlazlo.dev>
2026-05-03 19:05:20 +0200

    feat: migrate custom terminal.lua to snacks.terminal

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,6 +15,15 @@ require("snacks").setup({
       trace = "✎",
     },
   },
+  terminal = {
+    enabled = true,
+    win = {
+      border = "rounded",
+      width = 0.8,
+      height = 0.8,
+      position = "float",
+    },
+  },
   words = {
     enabled = true,
     debounce = 200,
@@ -22,6 +31,14 @@ require("snacks").setup({
     notify_end = false,
   },
 })
+
+vim.keymap.set("n", "<leader>t", function()
+  Snacks.terminal.toggle()
+end, { noremap = true, silent = true, desc = "Toggle floating terminal" })
+
+vim.keymap.set("t", "<Esc>", function()
+  Snacks.terminal.toggle()
+end, { noremap = true, silent = true, desc = "Close floating terminal" })
 
 vim.keymap.set("n", "<leader>n", function()
   Snacks.notifier.show_history()
